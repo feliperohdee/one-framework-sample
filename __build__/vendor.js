4aa199c229f44cebeaba564fe5c3bd9a2a21bc29
Felipe Rohde <feliperohdee@gmail.com>
2016-05-03 20:15:28 -0300

    updating assets according to current version

--- __build__/vendor.js
+++ __build__/vendor.js
@@ -1,2 +1,2 @@
-webpackJsonp([1],{0:function(n,t,c){n.exports=c(530)},530:function(n,t,c){"use strict";c(2),c(4),c(509),c(35)}});
+webpackJsonp([1],{0:function(n,t,c){n.exports=c(539)},539:function(n,t,c){"use strict";c(2),c(4),c(511),c(35)}});
 //# sourceMappingURL=vendor.js.map
