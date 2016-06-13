a7844d1621f295c68e15adb2fb6e882551bf9471
Felipe Rohde <feliperohdee@gmail.com>
2016-06-13 16:41:38 -0300

    trash removed

--- __serverBuild__/browser.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var app_1 = require('./app');
-app_1.one.bootstrap();
-//# sourceMappingURL=browser.js.map
--- __serverBuild__/mockedData.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var _ = require('lodash');
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = _.times(10, function (id) { return ({
-    id: id,
-    text: "task-" + id
-}); });
-//# sourceMappingURL=mockedData.js.map
